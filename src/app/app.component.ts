59793d578e035816f480b7c7a471da6419190a1a
SgD200 <sergiogomdu@hotmail.com>
2021-11-05 16:03:54 -0500

    con botones de busqueda

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,27 @@ import { ConsumoService } from './service/consumo.service';
 export class AppComponent {
   title = 'servicio';
   public page!: number;
+  buscando: number=0;
+  abuscar!:number;
 
   constructor(public consumo:ConsumoService){}
 
   ngOnInit(){
 
+  }
+
+  buscar(){
+    if(this.abuscar!=0){
+      this.buscando=this.abuscar;
+    }
+    if(this.abuscar>this.consumo.items.length || this.abuscar<=0){
+      alert('Este valor no existe')
+      this.buscando=0
+    }
+
+  }
+
+  volver(){
+    this.buscando=0;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxPaginationModule } from 'ngx-pagination';
 
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     NgxPaginationModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
 
   ],
   providers: [],
